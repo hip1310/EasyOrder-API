33a66446d353392fb84cc66eb4398690bdef54da
Harsh Patel <harshpatel131090@gmail.com>
2018-06-21 11:55:36 -0400

    Use dotenv to read environment variables

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@
 
 const express = require('express')
 
+// Configure environment
+require('./config/server/configureEnvironment')()
+
 /**
  * @description Configure the Express instance
  * @type {*}
@@ -14,6 +17,6 @@ const app = express()
  * @const listen
  * @description Starts the web server
  */
-app.listen(3005, () => {
-  console.log('EasyOrder API running on port 3005')
+app.listen(process.env.PORT, () => {
+  console.log(`EasyOrder API running on port ${process.env.PORT}`)
 })
